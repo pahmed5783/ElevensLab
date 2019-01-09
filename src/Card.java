75d6a442785d175156e56586e74371c64d299131
BT_1N3_13 <pahmed5783@bths.edu>
2019-01-09 11:35:56 -0500

    x  dead

--- src/Card.java
+++ src/Card.java
@@ -1,35 +1,48 @@
 public class Card {
+    // Fields
+    private String rank;
+    private String suit;
+    private int value;
 
-    private String rank, suit;
-
-    private int pointValue;
-
-    public Card(String r, String s, int pv){
-        rank = r;
-        suit = s;
-        pointValue = pv;
+    /**
+     * Main Constructor that builds a Card based on given parameters.
+     * @param rank String thar represents the rank of the Card.
+     * @param suit String thar represents the suit of the Card.
+     * @param value Integer that represents the value of the Card. (*Usually the rank but in Integer form*)
+     */
+    public Card (String rank, String suit, int value) {
+        this.rank = rank;
+        this.suit = suit;
+        this.value = value;
     }
 
-    public String getRank(){
-        return rank;
-    }
+    // Getters & Setters
+    public String getRank() { return rank; }
+    public void setRank(String rank) { this.rank = rank; }
 
-    public String getSuit(){
-        return suit;
-    }
+    public String getSuit() { return suit; }
+    public void setSuit(String suit) { this.suit = suit; }
 
-    public int getPointValue(){
-        return pointValue;
-    }
+    public int getPointValue() { return value; }
+    public void setPointValue(int pointValue) { this.value = pointValue; }
 
-    public boolean equals(Card otherCard){
-        if(this.getPointValue() == otherCard.getPointValue()){
+    /**
+     * Checks if a Cards value is equal to another Cards value.
+     * @param other Any other Card.
+     * @return True if both values are equal. Otherwise false.
+     */
+    public boolean equals(Card other) {
+        if (this.value == other.getPointValue()) {
             return true;
+        } else {
+            return false;
         }
-        return false;
     }
 
-    public String toString(){
-        return "Rank: " + rank + " | Suit: " + suit + " | Point Value: " + pointValue;
-    }
+    /**
+     * Generates a readable format for each Card.
+     * @return String that describes the card. (" 'Rank' - 'Suit' - Value ")
+     */
+    @Override
+    public String toString() { return this.rank + "-" + this.suit + "-" + this.value; }
 }
